b2578545daec8c29bc5a16bf3177391b85825494
Hey R You Are <55276676+Abab-bk@users.noreply.github.com>
2024-03-27 22:56:43 +0800

    实现LevelGenerator

---
+++ Scripts/Classes/LevelGenerator.cs
@@ -0,0 +1,168 @@
+using Godot;
+using System;
+using PowerSokoBan.Scripts.Classes.Factorys;
+using PowerSokoBan.Scripts.Prefabs;
+
+namespace PowerSokoBan.Scripts.Classes;
+
+[GlobalClass]
+public partial class LevelGenerator : Node2D
+{
+    [Export]
+    private TileMap _tileMap;
+
+    const int GirdSize = 64;
+    
+    public void GenerateLevel(string fileName)
+    {
+        _tileMap.Clear();   
+
+        var file = FileAccess.Open("res://Assets/Levels/" + fileName, FileAccess.ModeFlags.Read);
+        if (file == null)
+        {
+            return;
+        }
+        
+        var text = file.GetAsText();
+        file.Close();
+        
+        var lines = text.Split('\n');
+        var width = lines[0].Length;
+        var height = lines.Length - 1;
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < width; y++)
+            {
+                if (x < lines[y].Length)
+                {
+                    var letter = lines[y][x];
+                    MapBlockInfo obj = null;
+                    switch (letter)
+                    {
+                        case '#':
+                            obj = new MapBlockInfo()
+                                .SetMapBlockType(MapBlockInfo.MapBlockType.Wall);
+                            break;
+                        case '-':
+                            obj = new MapBlockInfo()
+                                .SetMapBlockType(MapBlockInfo.MapBlockType.HiddenWall);
+                            break;
+                        case '0':
+                            obj = new MapBlockInfo()
+                                .SetMapBlockType(MapBlockInfo.MapBlockType.FunctionBlock)
+                                .SetValue(0);
+                            break;
+                        case '1': case '2': case '3':
+                            obj = new MapBlockInfo()
+                                .SetMapBlockType(MapBlockInfo.MapBlockType.FunctionBlock)
+                                .SetValue(int.Parse(letter.ToString()));
+                            break;
+                        case 'S':
+                            obj = new MapBlockInfo()
+                                .SetMapBlockType(MapBlockInfo.MapBlockType.Swap);
+                            break;
+                        case '@':
+                            obj = new MapBlockInfo()
+                                .SetMapBlockType(MapBlockInfo.MapBlockType.Player);
+                            break;
+                        case '$':
+                            obj = new MapBlockInfo()
+                                .SetMapBlockType(MapBlockInfo.MapBlockType.Goal);
+                            break;
+                        case '.':
+                            continue;
+                    }
+
+                    if (obj == null)
+                    {
+                        continue;
+                    }
+
+                    if (obj.GetMapBlockType() == MapBlockInfo.MapBlockType.FunctionBlock)
+                    {
+                        FunctionBlock functionBlock = (FunctionBlock)ActorFactory.CreateActor(ActorType.FunctionBlockRed, obj.GetValue());
+                        AddChild(functionBlock);
+                        functionBlock.GlobalPosition = new Vector2I(x, y) * GirdSize;
+                        functionBlock.RulePosition();
+                        continue;
+                    }
+
+                    if (obj.GetMapBlockType() == MapBlockInfo.MapBlockType.Player)
+                    {
+                        Player player = (Player)ActorFactory.CreateActor(ActorType.Player);
+                        AddChild(player);
+                        player.GlobalPosition = new Vector2(x, y) * GirdSize;
+                        player.RulePosition();
+                        continue;
+                    }
+
+                    if (obj.GetMapBlockType() == MapBlockInfo.MapBlockType.Goal)
+                    {
+                        WinPoint winPoint = (WinPoint)ActorFactory.CreateActor(ActorType.WinPoint);
+                        AddChild(winPoint);
+                        winPoint.GlobalPosition = new Vector2(x, y) * GirdSize;
+                        winPoint.RulePosition();
+                        continue;
+                    }
+
+                    if (obj.GetMapBlockType() == MapBlockInfo.MapBlockType.HiddenWall)
+                    {
+                        _tileMap.SetCell(0, new Vector2I(x, y), 0);
+                    }
+
+                    // TODO: Add Swap.
+                    if (obj.GetMapBlockType() == MapBlockInfo.MapBlockType.Swap)
+                    {
+                        FunctionBlock functionBlock = (FunctionBlock)ActorFactory.CreateActor(ActorType.FunctionBlockRed, obj.GetValue());
+                        AddChild(functionBlock);
+                        functionBlock.GlobalPosition = new Vector2I(x, y) * GirdSize;
+                        functionBlock.RulePosition();
+                        continue;
+                    }
+                    
+                    // Vector2 tilePos = new Vector2(x * GirdSize, y * GirdSize);
+                    _tileMap.SetCell(0, new Vector2I(x, y), 0, new Vector2I(0, 0));
+                }
+            }
+        }
+    }
+}
+
+class MapBlockInfo
+{
+    public enum MapBlockType
+    {
+        Wall,
+        HiddenWall,
+        FunctionBlock,
+        Player,
+        Goal,
+        Swap,
+    }
+    
+    private MapBlockType _mapBlockType;
+    private int _value;
+
+    public MapBlockType GetMapBlockType()
+    {
+        return _mapBlockType;
+    }
+    
+    public int GetValue()
+    {
+        return _value;
+    }
+    
+    public MapBlockInfo SetMapBlockType(MapBlockType type)
+    {
+        _mapBlockType = type;
+        return this;
+    }
+
+    public MapBlockInfo SetValue(int value)
+    {
+        _value = value;
+        return this;
+    }
+}
--- Scripts/World.cs
+++ Scripts/World.cs
@@ -9,32 +9,10 @@ namespace PowerSokoBan.Scripts;
 
 public partial class World : Node2D
 {
-    [Export]
-    private Node2D _functionBlocks;
-
+    [Export] private LevelGenerator _levelGenerator;
+    
     public override void _Ready()
     {
-        foreach (var mark in _functionBlocks.GetChildren())
-        {
-            if (mark is Marker2D == false)
-            {
-                continue;
-            }
-            
-            var mark2D = (Marker2D) mark;
-            AddFunctionBlocks(mark2D.GlobalPosition, 2, ActorType.FunctionBlockBlue, Actor.Direction.Down);
-        }
-    }
-
-    private void AddFunctionBlocks(Vector2 pos, int value, ActorType actorType, Actor.Direction direction)
-    {
-        var functionBlock = ActorFactory.CreateActor(actorType, value, direction);
-        if (functionBlock == null)
-        {
-            return;
-        }
-        _functionBlocks.AddChild(functionBlock);
-        functionBlock.GlobalPosition = pos;
-        functionBlock.RulePosition();
+        _levelGenerator.GenerateLevel("intro1b.txt");
     }
 }
