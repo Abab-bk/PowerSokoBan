0bb060e4f7ded908fa8d6149524d8d4610c32dbb
Hey R You Are <55276676+Abab-bk@users.noreply.github.com>
2024-04-04 01:33:19 +0800

    添加隐私政策、TapTap登录、口袋工厂插件

--- Scripts/StartCut.cs
+++ Scripts/StartCut.cs
@@ -5,14 +5,46 @@ namespace PowerSokoBan.Scripts;
 public partial class StartCut : Control
 {
     [Export] private AnimationPlayer _animationPlayer;
+    [Export] private TextureButton _loginBtn;
 
+    private Node _taptap;
+    
     public override void _Ready()
     {
+        _taptap = GetNode<Node>("/root/GodotTapTap");
+        _taptap.Connect("onLoginResult", new Callable(this, nameof(OnLoginResult)));
+        _taptap.Call("onAntiAddictionCallback", new Callable(this, nameof(OnAntiAddictionCallback)));
+        
         _animationPlayer.AnimationFinished += delegate
         {
-            SceneManager.ChangeSceneTo(this, "res://Scenes/StartMenu.tscn");
+            if ((bool)_taptap.Call("isLogin"))
+            {
+                CheckAntiAddiction();
+            }
         };
         
         _animationPlayer.Play("run");
+
+        _loginBtn.Pressed += delegate
+        {
+            _taptap.Call("tap_login");
+        };
+    }
+
+    private void OnLoginResult(int code, string json)
+    {
+        GD.Print("登录结果：", code, json);
+        CheckAntiAddiction();
+    }
+
+    private void OnAntiAddictionCallback(int code)
+    {
+        if (code != 500) return;
+        SceneManager.ChangeSceneTo(this, "res://Scenes/StartMenu.tscn");
+    }
+    
+    private void CheckAntiAddiction()
+    {
+        _taptap.Call("quickCheck");
     }
 }
